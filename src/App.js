8a782298e647b61808b544087ae97374f46cc9f6
namefe <smc9102@naver.com>
2025-04-19 00:11:15 +0900

    0419 1차

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
 
+import Aos from 'aos';
 import './App.css';
 import { motion } from "framer-motion";
+import { useEffect } from 'react';
+import "aos/dist/aos.css";
 
 function App() {
 
 
+  useEffect(() => {
+    Aos.init({
+      duration: 800, // 애니메이션 시간 (ms)
+      once: false,    // 한 번만 작동할지 여부
+    });
+  }, []);
+
+
   const fadeInVariants = {
-    hidden: { opacity: 0, x: 50 },
+    hidden: { opacity: 0, y: 20 },
     visible: (i) => ({
       opacity: 1,
-      x: 0,
-      transition: { 
+      y: 0,
+      transition: {
         delay: i * 0.1,
-         duration: 0.6, 
-         ease: "easeOut" }
+        duration: 0.5,
+      },
     }),
   };
+  const imageList = [
+    "sm6.png", "sm12.png", "sm8.png", "sm9.png",
+    "sm7.png", "sm1.png", "sm10.png", "sm11.png",
+    "sm2.png", "sm3.png", "sm4.png", "sm5.png"
+  ];
 
   const Header = () => (
     <header className="absolute top-0 left-0 w-full flex justify-between items-center p-4 sm:p-6 z-10">
@@ -85,52 +101,53 @@ function App() {
           <img className='country absolute top-[50%] left-[50%]' src={process.env.PUBLIC_URL + '/sec2_01.png'}/>
       </section>
     {/*------------------------------------------------------------- section2 끝 --------------------------------------------*/}
-    <div className="sec4bg w-full h-full text-black font-bold" style={{backgroundImage:`url(${process.env.PUBLIC_URL}/sec3_bg.png)`}}>
-    <section>
-      <div/>
-        <div className='m-32'>
-        <div className='txt text-[40px]'>
-          <div className='inline-block'>SM Entertainment는 1995년 설립 이후 </div>
-          <div className='txt-image inline-block'>
-          <img src={process.env.PUBLIC_URL + '/sec3_01.png'}></img>
-          </div>
-        </div>
-        <div className='txt text-[40px]'>
-           K-POP을 선도하며 글로벌 시장을 개척해왔습니다.
-        </div>
-        <div className='txt text-[40px]'>
-          최고의 인재와 퍼포먼스로 세계에
-          <div className='txt-image inline-block'>
-          <img src={process.env.PUBLIC_URL + '/sec3_02.png'}></img>
-          </div>
-           '한류'를 알리며
-        </div>
-        <div className='txt text-[40px]'>
-          <div className='txt-image inline-block'>
-            <img className='w-20 h-20' src={process.env.PUBLIC_URL + '/sec3_03.png'}></img>
-          </div>
-          글로벌 1등 기업으로 도약하겠습니다.
-        </div>
-        </div>
-      </section>
+    <div
+  className="sec4bg w-full h-full text-black font-bold"
+  style={{ backgroundImage: `url(${process.env.PUBLIC_URL}/sec3_bg.png)` }}
+>
+  <section>
+    <div className="m-32 text-[20px] leading-relaxed text-center">
+      <span className="text-justify 2xl:text-[55px]">
+        SM Entertainment는 1995년 설립 이후
+        <img
+          src={`${process.env.PUBLIC_URL}/sec3_01.png`}
+          alt="icon1"
+          className="inline w-21 h-6 2xl:w-40 2xl:h-14 align-middle mx-1"
+        />
+        <br/>K-POP을 선도하며 글로벌 시장을 개척해왔습니다.<br/> 최고의 인재와 퍼포먼스로 세계에
+        <img
+          src={`${process.env.PUBLIC_URL}/sec3_02.png`}
+          alt="icon2"
+          className="inline w-21 h-6 2xl:w-40 2xl:h-14 align-middle mx-1"
+        />
+        ‘한류’를 알리며
+        <img
+          src={`${process.env.PUBLIC_URL}/sec3_03.png`}
+          alt="icon3"
+          className="inline w-12 h-12 2xl:w-20 2xl:h-20 align-middle mx-1"
+        />
+        글로벌 1등 기업으로 도약하겠습니다.
+      </span>
     </div>
+  </section>
+</div>
     {/*------------------------------------------------------------- section4 끝 --------------------------------------------*/}
 
-    <section className="sec4bg w-full text-white font-bold" style={{backgroundImage:`url(${process.env.PUBLIC_URL}/sec4_bg.png)`}}>
-        <div className='sec5'>
-          <div className='sec5txt'>
-            <div className='sec5txt1'>SM Entertainment는</div>
-            <div className='sec5txt2'>K-Culture의 미래를 설계합니다.</div>
+    <section className="sec4bg w-auto text-white font-bold bg-contain" style={{backgroundImage:`url(${process.env.PUBLIC_URL}/sec4_bg.png)`}}>
+        <div className='relative'>
+          <div className='sec5txt '>
+            <div className='sec5txt1 lg:text-2xl xl:text-[20px]'>SM Entertainment는</div>
+            <div className='sec5txt2 lg:text-2xl xl:text-[40px]'>K-Culture의 미래를 설계합니다.</div>
             </div>
-            <img className='sec5img1' src={process.env.PUBLIC_URL + '/sec4_01.png'}></img>
-            <img className='sec5img2' src={process.env.PUBLIC_URL + '/sec4_02.png'}></img>
-            <img className='sec5img3' src={process.env.PUBLIC_URL + '/sec4_03.png'}></img>
-            <img className='sec5img4' src={process.env.PUBLIC_URL + '/sec4_01.png'}></img>
-            <img className='sec5img5' src={process.env.PUBLIC_URL + '/sec4_02.png'}></img>
-            <img className='sec5img6' src={process.env.PUBLIC_URL + '/sec4_03.png'}></img>
-            <img className='sec5img7' src={process.env.PUBLIC_URL + '/sec4_01.png'}></img>
-            <img className='sec5img8' src={process.env.PUBLIC_URL + '/sec4_02.png'}></img>
-            <img className='sec5img9' src={process.env.PUBLIC_URL + '/sec4_03.png'}></img>
+            <div className='sec5'>
+            <img className='sec5img1 w-1/3 h-auto object-contain'data-aos="fade-up" src={process.env.PUBLIC_URL + '/sec4_01.png'}></img>
+            <img className='sec5img2 w-1/3 h-auto object-contain'data-aos="fade-up" src={process.env.PUBLIC_URL + '/sec4_03.png'}></img>
+            <img className='sec5img3 w-1/3 h-auto object-contain'data-aos="fade-up" src={process.env.PUBLIC_URL + '/sec4_01.png'}></img>
+            <img className='sec5img4 w-1/3 h-auto object-contain'data-aos="fade-up" src={process.env.PUBLIC_URL + '/sec4_03.png'}></img>
+            <img className='sec5img5 w-1/3 h-auto object-contain'data-aos="fade-up" src={process.env.PUBLIC_URL + '/sec4_01.png'}></img>
+            <img className='sec5img6 w-1/3 h-auto object-contain'data-aos="fade-up" src={process.env.PUBLIC_URL + '/sec4_03.png'}></img>
+            <img className='sec5img7 w-1/2 h-auto object-contain'data-aos="fade-up" src={process.env.PUBLIC_URL + '/sec4_02.png'}></img>
+            </div>
             </div>
       </section>
     {/* -------------------------------------------------------------------section5 끝-------------------------------------------- */}
@@ -231,104 +248,32 @@ function App() {
     <div className="text-sm mt-4">SM의 빛나는 성장을 함께한 주요 아티스트</div>
   </div>
   <div className="grid grid-cols-4 gap-4">
-    
-    <motion.div  custom={0} initial="hidden" whileInView="visible" viewport={{ once: true }} variants={fadeInVariants}
-            className="relative group overflow-hidden aspect-[4/3]">
-      <img src={process.env.PUBLIC_URL + '/sm6.png'} className="object-cover w-full h-full transition duration-300 group-hover:brightness-50 group-hover:scale-105" />
-      <div style={{ cursor: "url('/hovercursor_32x32.png') 0 0, auto" }} className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition duration-300">
-        <a className="text-white font-bold text-[20px]">VIEW MUSIC VIDEO ↗</a>
+        {imageList.map((img, index) => (
+          <motion.div
+            key={img}
+            custom={index}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            variants={fadeInVariants}
+            className="relative group overflow-hidden aspect-[4/3]"
+          >
+            <img
+              src={`${process.env.PUBLIC_URL}/${img}`}
+              alt={img}
+              className="object-cover w-full h-full transition duration-300 group-hover:brightness-50 group-hover:scale-105"
+            />
+            <div
+              style={{ cursor: "url('/hovercursor_32x32.png') 0 0, auto" }}
+              className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition duration-300"
+            >
+              <a className="text-white font-bold text-[10px]">
+                VIEW MUSIC VIDEO ↗
+              </a>
+            </div>
+          </motion.div>
+        ))}
       </div>
-    </motion.div>
-
-    <motion.div custom={1} initial="hidden" whileInView="visible" viewport={{ once: true }} variants={fadeInVariants}
-    className="relative group overflow-hidden aspect-[4/3]">
-      <img src={process.env.PUBLIC_URL + '/sm12.png'} className="object-cover w-full h-full transition duration-300 group-hover:brightness-50 group-hover:scale-105" />
-      <div style={{ cursor: "url('/hovercursor_32x32.png') 0 0, auto" }} className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition duration-300">
-        <a className="text-white font-bold text-[20px]">VIEW MUSIC VIDEO ↗</a>
-      </div>
-    </motion.div>
-
-    <motion.div custom={2} initial="hidden" whileInView="visible" viewport={{ once: true }} variants={fadeInVariants}
-    className="relative group overflow-hidden aspect-[4/3]">
-      <img src={process.env.PUBLIC_URL + '/sm8.png'} className="object-cover w-full h-full transition duration-300 group-hover:brightness-50 group-hover:scale-105" />
-      <div style={{ cursor: "url('/hovercursor_32x32.png') 0 0, auto" }} className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition duration-300">
-        <a className="text-white font-bold text-[20px]">VIEW MUSIC VIDEO ↗</a>
-      </div>
-    </motion.div>
-
-    <motion.div custom={3} initial="hidden" whileInView="visible" viewport={{ once: true }} variants={fadeInVariants}
-    className="relative group overflow-hidden aspect-[4/3]">
-      <img src={process.env.PUBLIC_URL + '/sm9.png'} className="object-cover w-full h-full transition duration-300 group-hover:brightness-50 group-hover:scale-105" />
-      <div style={{ cursor: "url('/hovercursor_32x32.png') 0 0, auto" }} className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition duration-300">
-        <a className="text-white font-bold text-[20px]">VIEW MUSIC VIDEO ↗</a>
-      </div>
-    </motion.div>
-
-    <motion.div custom={4} initial="hidden" whileInView="visible" viewport={{ once: true }} variants={fadeInVariants}
-    className="relative group overflow-hidden aspect-[4/3]">
-      <img src={process.env.PUBLIC_URL + '/sm7.png'} className="object-cover w-full h-full transition duration-300 group-hover:brightness-50 group-hover:scale-105" />
-      <div style={{ cursor: "url('/hovercursor_32x32.png') 0 0, auto" }} className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition duration-300">
-        <a className="text-white font-bold text-[20px]">VIEW MUSIC VIDEO ↗</a>
-      </div>
-    </motion.div>
-
-    <motion.div custom={5} initial="hidden" whileInView="visible" viewport={{ once: true }} variants={fadeInVariants}
-    className="relative group overflow-hidden aspect-[4/3]">
-      <img src={process.env.PUBLIC_URL + '/sm1.png'} className="object-cover w-full h-full transition duration-300 group-hover:brightness-50 group-hover:scale-105" />
-      <div style={{ cursor: "url('/hovercursor_32x32.png') 0 0, auto" }} className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition duration-300">
-        <a className="text-white font-bold text-[20px]">VIEW MUSIC VIDEO ↗</a>
-      </div>
-    </motion.div>
-
-    <motion.div custom={6} initial="hidden" whileInView="visible" viewport={{ once: true }} variants={fadeInVariants}
-     className="relative group overflow-hidden aspect-[4/3]">
-      <img src={process.env.PUBLIC_URL + '/sm10.png'} className="object-cover w-full h-full transition duration-300 group-hover:brightness-50 group-hover:scale-105" />
-      <div style={{ cursor: "url('/hovercursor_32x32.png') 0 0, auto" }} className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition duration-300">
-        <a className="text-white font-bold text-[20px]">VIEW MUSIC VIDEO ↗</a>
-      </div>
-    </motion.div>
-
-    <motion.div custom={7} initial="hidden" whileInView="visible" viewport={{ once: true }} variants={fadeInVariants}
-     className="relative group overflow-hidden aspect-[4/3]">
-      <img src={process.env.PUBLIC_URL + '/sm11.png'} className="object-cover w-full h-full transition duration-300 group-hover:brightness-50 group-hover:scale-105" />
-      <div style={{ cursor: "url('/hovercursor_32x32.png') 0 0, auto" }} className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition duration-300">
-        <a className="text-white font-bold text-[20px]">VIEW MUSIC VIDEO ↗</a>
-      </div>
-    </motion.div>
-
-    <motion.div custom={8} initial="hidden" whileInView="visible" viewport={{ once: true }} variants={fadeInVariants}
-    className="relative group overflow-hidden aspect-[4/3]">
-      <img src={process.env.PUBLIC_URL + '/sm2.png'} className="object-cover w-full h-full transition duration-300 group-hover:brightness-50 group-hover:scale-105" />
-      <div style={{ cursor: "url('/hovercursor_32x32.png') 0 0, auto" }} className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition duration-300">
-        <a className="text-white font-bold text-[20px]">VIEW MUSIC VIDEO ↗</a>
-      </div>
-    </motion.div>
-
-    <motion.div custom={9} initial="hidden" whileInView="visible" viewport={{ once: true }} variants={fadeInVariants}
-    className="relative group overflow-hidden aspect-[4/3]">
-      <img src={process.env.PUBLIC_URL + '/sm3.png'} className="object-cover w-full h-full transition duration-300 group-hover:brightness-50 group-hover:scale-105" />
-      <div style={{ cursor: "url('/hovercursor_32x32.png') 0 0, auto" }} className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition duration-300">
-        <a className="text-white font-bold text-[20px]">VIEW MUSIC VIDEO ↗</a>
-      </div>
-    </motion.div>
-
-    <motion.div custom={10} initial="hidden" whileInView="visible" viewport={{ once: true }} variants={fadeInVariants}
-    className="relative group overflow-hidden aspect-[4/3]">
-      <img src={process.env.PUBLIC_URL + '/sm4.png'} className="object-cover w-full h-full transition duration-300 group-hover:brightness-50 group-hover:scale-105" />
-      <div style={{ cursor: "url('/hovercursor_32x32.png') 0 0, auto" }} className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition duration-300">
-        <a className="text-white font-bold text-[20px]">VIEW MUSIC VIDEO ↗</a>
-      </div>
-    </motion.div>
-
-    <motion.div custom={11} initial="hidden" whileInView="visible" viewport={{ once: true }} variants={fadeInVariants}
-     className="relative group overflow-hidden aspect-[4/3]">
-      <img src={process.env.PUBLIC_URL + '/sm5.png'} className="object-cover w-full h-full transition duration-300 group-hover:brightness-50 group-hover:scale-105" />
-      <div style={{ cursor: "url('/hovercursor_32x32.png') 0 0, auto" }} className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition duration-300">
-        <a className="text-white font-bold text-[20px]">VIEW MUSIC VIDEO ↗</a>
-      </div>
-    </motion.div>
-
-  </div>
 </section>
     {/*SECTION6 끝 */}
 
@@ -391,7 +336,7 @@ function App() {
     {/*-------------------------------------------------------------------FOOTER 시작--------------------------------------------- */}
     <footer>
       <section className=" w-full h-auto bg-black text-white  px-6 py-12">
-        <div className='footer1 inline-block'>
+        <div className='footer1 xl:flex xl:justify-between'>
       <div>
       <img src={process.env.PUBLIC_URL + '/smlogo.png'}/>
       </div>
@@ -402,13 +347,13 @@ function App() {
           <div className='opacity-80'>이메일: smeir@smtown.com</div>
         </div>
         <div>
-          <h3 className='font-bold mt-4'>OUR BUSINESS</h3>
-          <div className='opacity-80'>SM Entertainment operates across multiple business<br/>sectors within the entertainment industry.</div>
+          <h3 className='font-bold mt-4 hidden 2xl:block'>OUR BUSINESS</h3>
+          <div className='opacity-80 hidden 2xl:block'>SM Entertainment operates across multiple business<br/>sectors within the entertainment industry.</div>
         </div>
       </div>
       </div>
-      <div className="footer1 inline-block border-t border-gray-700 my-8">
-      <div>
+      <div className="footer1 inline-block border-t border-gray-700 my-8 w-full xl:flex xl:justify-between">
+      <div className='block'>
         <h3 className='font-bold mt-4'>OUR COMMUNITY</h3>
         <div className='flex flex-end'>
         <img className='m-2' src={process.env.PUBLIC_URL + '/icon3.png'}/>
